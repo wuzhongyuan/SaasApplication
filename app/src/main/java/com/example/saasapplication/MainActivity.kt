9c6a7aefa12404d48dcee0ab7a57eee8bf63c631
wuzhongyuan <37972401@qq.com>
2021-07-07 15:52:49 +0800

    调试 VersionName 和 VersionCode。

--- app/src/main/java/com/example/saasapplication/MainActivity.kt
+++ app/src/main/java/com/example/saasapplication/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.saasapplication
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.TextView
 import com.example.saasapplication.R
 
 class MainActivity : AppCompatActivity() {
@@ -10,5 +11,9 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         Log.d("wzy", "onCreate: packageName = $packageName")
+        val textViewName = findViewById<TextView>(R.id.tv_version_name)
+        textViewName.text = BuildConfig.VERSION_NAME
+        val textViewCode= findViewById<TextView>(R.id.tv_version_code)
+        textViewCode.text = BuildConfig.VERSION_CODE.toString()
     }
 }
